50f493b1cc3fae0a976de60ecc69ebfbe05d7d87
dan <chezydan@gmail.com>
2022-06-30 23:15:01 +0300

    Updates

---
+++ precache-manifest.f0f6a24cf8afe6df882d564c27270db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9310871a870ead07f459",
+    "url": "/react-todo-project.-/static/css/main.b0f5a202.chunk.css"
+  },
+  {
+    "revision": "9310871a870ead07f459",
+    "url": "/react-todo-project.-/static/js/main.9310871a.chunk.js"
+  },
+  {
+    "revision": "86a1dbeb4b6983715a80",
+    "url": "/react-todo-project.-/static/js/1.86a1dbeb.chunk.js"
+  },
+  {
+    "revision": "08f1f7758984fe29f9d9",
+    "url": "/react-todo-project.-/static/js/runtime~main.08f1f775.js"
+  },
+  {
+    "revision": "1c51b5b213bf7c04578c93a75e5acfe2",
+    "url": "/react-todo-project.-/index.html"
+  }
+];
